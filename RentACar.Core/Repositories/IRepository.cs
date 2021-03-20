bd1f803f800f610e53aac6c3d115d71f01cf1357
Necdet UYGUR <necdet.uygur@gmail.com>
2021-03-20 19:26:13 +0300

    Add project files.

---
+++ RentACar.Core/Repositories/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace RentACar.Core.Repositories
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        Task<TEntity> GetByIdAsync(int id);
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<IEnumerable<TEntity>> Where(Expression<Func<TEntity, bool>> predicate);
+        Task<TEntity> SingleOrDefaultAsync(Expression<Func<TEntity, bool>> predicate);
+        Task AddAsync(TEntity entity);
+        Task AddRangeAsync(IEnumerable<TEntity> entities);
+        void Remove(TEntity entity);
+        void RemoveRange(IEnumerable<TEntity> entities);
+        TEntity Update(TEntity entity);
+    }
+}
---
+++ RentACar.Core/UnitOfWorks/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace RentACar.Core.UnitOfWorks
+{
+    public interface IUnitOfWork
+    {
+        Task CommitAsync();
+        void Commit();
+    }
+}
