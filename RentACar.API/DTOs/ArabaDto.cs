e0b5ca2b665228cb6805256179f4d456d8e59a05
Necdet UYGUR <necdet.uygur@gmail.com>
2021-03-21 15:23:57 +0300

    Araba entity modeli oluşturuldu.

---
+++ RentACar.API/DTOs/ArabaDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RentACar.API.DTOs
+{
+    public class ArabaDto
+    {
+        public int ArabaID { get; set; }
+        [Required]
+        public string Plaka { get; set; }
+        public string Marka { get; set; }
+        public string Model { get; set; }
+        public int UretimYili { get; set; }
+        public string YakitTuru { get; set; }
+        public string EhliyetSinifi { get; set; }
+        public int KoltukSayisi { get; set; }
+    }
+}
